65a34dd57ff2e58da683247cc33c239784cf89b7
Wes Steiner <wwes@ksu.edu>
2022-06-21 23:28:56 +0000

    final commit for milestone 2

---
+++ static/site.js
@@ -0,0 +1,29 @@
+
+function displayCards(data){ 
+  for(let i=0; i<data.length; i++){
+    var json_data = data[i];
+    var display = document.getElementById("view");
+    var lat = json_data.lat;
+    var lng = json_data.lng;
+    var map = document.createElement("img")
+    map.src = "http://www.mapquestapi.com/staticmap/v4/getmap?key=YiIbGrPqh1Gq6pmFKTopo6uTTTus3bON&size=600,400&zoom=16&center="+lat+","+lng+"&pois=poi-blue,"+lat+","+lng+",0,0";
+    var name = document.createElement("p"); 
+    name.textContent = json_data.name;
+    var iDiv = document.createElement('div');
+    display.appendChild(iDiv);
+    iDiv.appendChild(map);
+    iDiv.appendChild(name);
+  } 
+}
+
+function useXHR(){
+  const xhr = new XMLHttpRequest();
+  xhr.addEventListener('load', ()=>{
+      displayCards(JSON.parse(xhr.responseText));
+  });
+
+  const url = "./box-locations.json";
+  xhr.open("GET", url);
+  xhr.send();
+}
+useXHR();
